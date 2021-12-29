060d8b45327db162c4a77358250618773f5371ce
Peyton Randolph <me@peytn.com>
2021-12-28 19:05:19 -0600

    add error

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,3 +1,7 @@
+use std::convert::TryInto;
+use solana_program::program_error::ProgramError;
+
+use crate::error::EscrowError::InvalidInstruction;
 
 pub enum EscrowInstruction {
 
@@ -17,5 +21,28 @@ pub enum EscrowInstruction {
     InitEscrow {
         /// The amount party A excepts to receive of token Y.
         amount: u64
+    }
+}
+
+impl EscrowInstruction {
+    /// Unpacks a byte buffer into a [EscrowInstruction](enum.EscrowInstruction.html).
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
+
+        Ok(match tag {
+            0 => Self::InitEscrow {
+                amount: Self::unpack_amount(rest)?,
+            },
+            _ => return Err(InvalidInstruction.into()),
+        })
+    }
+
+    fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
+        let amount = input
+            .get(..8)
+            .and_then(|slice| slice.try_into().ok())
+            .map(u64::from_le_bytes)
+            .ok_or(InvalidInstruction)?;
+        Ok(amount)
     }
 }
